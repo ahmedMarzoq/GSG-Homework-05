b60f203e346f4e1701df6dbda95275acd7e3b1d4
ahmed marzoq <you@example.com>
2020-09-30 12:44:06 +0300

    update-js-file

--- js/index.js
+++ js/index.js
@@ -1,3 +1,37 @@
 // Here you can access the colors variable
 // have fun
 
+//Function that return random value from colors array
+function randomValue() {
+    return colors[Math.floor(Math.random() * colors.length)];
+}
+
+console.log(randomValue());
+
+// get elements
+let button = document.querySelector('div.control > button');
+let inputColor = document.querySelector('#color');
+let paintMe = document.querySelector('#pallete');
+
+console.log(button);
+console.log(inputColor);
+console.log(paintMe);
+
+// button lestner
+button.addEventListener('click', function (event) {
+    let randomColor = randomValue();
+    inputColor.value = randomColor;
+    inputColor.style.color = randomColor;
+    paintMe.style.backgroundColor = randomColor;
+});
+
+// inputColor lestner
+inputColor.addEventListener('keyup', function (event) {
+    console.value = event.target.value;
+    if(!inputColor.value.includes("#")){
+        inputColor.value = "#" + event.target.value;
+    }
+    
+    inputColor.style.color = event.target.value;
+    paintMe.style.backgroundColor = event.target.value;
+});
